cff46778580b268e263eb00e7ef0baef51359a14
Vishal <vishal.pandya@softwebsolutions.com>
2020-03-11 19:53:20 +0530

    Commit

--- SwitchEx/src/main/java/PaymentPage.java
+++ SwitchEx/src/main/java/PaymentPage.java
@@ -2,5 +2,7 @@ public class PaymentPage {
 
     public static void main(String[] args) {
         System.out.println("Updated by Vishal");
+
+        System.out.println("Module updated on 3rd March: Vishal");
     }
 }
