82901ffd4123bf94265b1dbd42a178dfd36c0e40
Vishal <vishal.pandya@softwebsolutions.com>
2020-03-11 19:05:30 +0530

    Commit

--- SwitchEx/src/main/java/Profile.java
+++ SwitchEx/src/main/java/Profile.java
@@ -1,6 +1,9 @@
 public class Profile {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("This is ProfilePage");
+        System.out.println("Changes are done");
+
     }
 }
